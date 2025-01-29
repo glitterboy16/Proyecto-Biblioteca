a6e467c5c8c2206104cee380a7de6c86c023ddfd
Angel <villorinaangelandres@gmail.com>
2025-01-29 20:37:15 +0100

    comit con los libros

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 public class Biblioteca {
     public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        GestorUsuarios gestor = new GestorUsuarios();
-        gestor.usuarioNuevo(new Usuarios("Angel", "1234", TipoUsuario.ADMIN));
-        gestor.usuarioNuevo(new Usuarios("Pablo", "1234", TipoUsuario.ADMIN));
-        gestor.usuarioNuevo(new Usuarios("Jose", "1234", TipoUsuario.USER));
-        gestor.usuarioNuevo(new Usuarios("Marta", "rodasbichogrande", TipoUsuario.USER));
-        System.out.println(menuAdmin());
-        //menuPrincipal(gestor);
+        GestorUsuarios gestorU = new GestorUsuarios();
+        gestorU.usuarioNuevo(new Usuarios("Angel", "1234", TipoUsuario.ADMIN));
+        gestorU.usuarioNuevo(new Usuarios("Pablo", "1234", TipoUsuario.ADMIN));
+        gestorU.usuarioNuevo(new Usuarios("Jose", "1234", TipoUsuario.USER));
+        gestorU.usuarioNuevo(new Usuarios("Marta", "rodasbichogrande", TipoUsuario.USER));
+
+        //
+        GestorLibros gestorL = new GestorLibros();
+        gestorL.nuevoLibro(new Libro("El señor de los anillos", "J.R.R. Tolkien", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("El hobbit", "J.R.R. Tolkien", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("Harry Potter y la piedra filosofal", "J.K. Rowling", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("Harry Potter y la camara secreta", "J.K. Rowling", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("Harry Potter y el prisionero de Azkaban", "J.K. Rowling", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("Harry Potter y el caliz de fuego", "J.K. Rowling", Categoria.FANTASIA));
+        gestorL.nuevoLibro(new Libro("Meditaciones", "Marco Aurelio", Categoria.FILOSOFIA));
+        gestorL.nuevoLibro(new Libro("La republica", "Platon", Categoria.FILOSOFIA));
+        gestorL.nuevoLibro(new Libro("Critica de la razon pura", "Immanuel Kant", Categoria.FILOSOFIA));
+        gestorL.nuevoLibro(new Libro("El arte de la guerra", "Sun Tzu", Categoria.FILOSOFIA));
+        gestorL.nuevoLibro(new Libro("La divina comedia", "Dante Alighieri", Categoria.POESIA));
+        gestorL.nuevoLibro(new Libro("Rimas y leyendas", "Gustavo Adolfo Becquer", Categoria.POESIA));
+        gestorL.nuevoLibro(new Libro("Cien años de soledad", "Gabriel Garcia Marquez", Categoria.TERROR));
+        
+        menuPrincipal(gestorU);
     }
 
     public static void menuPrincipal(GestorUsuarios gestor){
@@ -65,7 +81,7 @@ public class Biblioteca {
         String reset = "\u001B[0m";
         String menu= tVerde +"-----------------------------------------------------------------\n"
                             + "|                                                              |\n"
-                            + "|      "+fBlanco+"Bienvenido a tus posibilidades como Usuario" + reset + tVerde+"             |\n"
+                            + "|      "+fBlanco+"Bienvenido a tus posibilidades como Admin" + reset + tVerde+"               |\n"
                             + "|                                                              |\n"
                             + "|--------------------------------------------------------------|\n"
                             + "|      1. Buscar libros                                        |\n" 
---
+++ src/EstadoLibro.java
@@ -0,0 +1,4 @@
+public enum EstadoLibro {
+    DISPONIBLE,
+    PRESTADO
+}
