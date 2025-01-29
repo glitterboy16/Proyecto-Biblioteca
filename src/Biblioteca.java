46e2803da1d71e442fdfeb14a106319d6dead6fc
Nuel2004 <manurodaspinilla@gmail.com>
2025-01-29 04:28:17 +0100

    todos los menus listos

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -7,8 +7,8 @@ public class Biblioteca {
         gestor.usuarioNuevo(new Usuarios("Pablo", "1234", TipoUsuario.ADMIN));
         gestor.usuarioNuevo(new Usuarios("Jose", "1234", TipoUsuario.USER));
         gestor.usuarioNuevo(new Usuarios("Marta", "rodasbichogrande", TipoUsuario.USER));
-    
-        menuPrincipal(gestor);
+        System.out.println(menuAdmin());
+        //menuPrincipal(gestor);
     }
 
     public static void menuPrincipal(GestorUsuarios gestor){
@@ -30,14 +30,14 @@ public class Biblioteca {
         String fBlanco = "\u001B[32;40m";
         String reset = "\u001B[0m";
         String menu= tVerde +"-----------------------------------\n"
-                + "|                                 |\n"
-                + "|    "+fBlanco+"Bienvenido a Byte & book" + reset + tVerde+"     |\n"
-                + "|                                 |\n"
-                + "|---------------------------------|\n"
-                + "|      1. Iniciar sesión          |\n" 
-                + "|      2. Salir                   |\n"
-                + "-----------------------------------\n"
-                + "Elija una opción:\n" + reset;
+                            + "|                                 |\n"
+                            + "|    "+fBlanco+"Bienvenido a Byte & book" + reset + tVerde+"     |\n"
+                            + "|                                 |\n"
+                            + "|---------------------------------|\n"
+                            + "|      1. Iniciar sesión          |\n" 
+                            + "|      2. Salir                   |\n"
+                            + "-----------------------------------\n"
+                            + "Elija una opción:\n" + reset;
         return menu;
     }
     
@@ -50,30 +50,57 @@ public class Biblioteca {
         Usuarios usuinisesion = gestor.inicioSesionUsuarios(nombreini, contrasenaini);
         if (usuinisesion!=null) {
             if (usuinisesion.getTipoUsuario()==TipoUsuario.ADMIN) {
-                System.out.println("menu admin");//poner el menu admin (otra funcion)
+                System.out.println(menuAdmin());
             }else{
-                System.out.println("menu user");//poner menu de user (otra funcion)
+                System.out.println(menuUser());
             }
         }else{
             System.out.println("ta mal");
         }
     }
 
+    public static String menuAdmin(){
+        String tVerde = "\u001B[32m";
+        String fBlanco = "\u001B[32;40m";
+        String reset = "\u001B[0m";
+        String menu= tVerde +"-----------------------------------------------------------------\n"
+                            + "|                                                              |\n"
+                            + "|      "+fBlanco+"Bienvenido a tus posibilidades como Usuario" + reset + tVerde+"             |\n"
+                            + "|                                                              |\n"
+                            + "|--------------------------------------------------------------|\n"
+                            + "|      1. Buscar libros                                        |\n" 
+                            + "|      2. Mostrar los libros disponibles                       |\n"
+                            + "|      3. Realizar prestamo de libro                           |\n"
+                            + "|      4. Agregar libro                                        |\n"
+                            + "|      5. Eliminar libro                                       |\n"
+                            + "|      6. Registrar nuevos usuarios                            |\n"
+                            + "|      7. Mostrar libros prestados                             |\n"
+                            + "|      8. Mostrar numero de prestamos totales y activos        |\n"
+                            + "|      9. Lista de libros mas prestados                        |\n"
+                            + "|      10. Mostrar que usuario tiene mas prestamos activos     |\n"
+                            + "|      11. Salir                                               |\n"
+                            + "----------------------------------------------------------------\n"
+                            + "Elija una opción:\n" + reset;
+        return menu;
+    }
+
     public static String menuUser(){
         String tVerde = "\u001B[32m";
         String fBlanco = "\u001B[32;40m";
         String reset = "\u001B[0m";
-        String menu= tVerde +"-----------------------------------\n"
-                + "|                                 |\n"
-                + "|    "+fBlanco+"Bienvenido a tus posibilidades" + reset + tVerde+"     |\n"
-                + "|                                 |\n"
-                + "|---------------------------------|\n"
-                + "|      1. Iniciar sesión          |\n" 
-                + "|      2. Salir                   |\n"
-                + "-----------------------------------\n"
-                + "Elija una opción:\n" + reset;
+        String menu= tVerde +"--------------------------------------------------\n"
+                            + "|                                                |\n"
+                            + "|  "+fBlanco+"Bienvenido a tus posibilidades como Usuario" + reset + tVerde+"   |\n"
+                            + "|                                                |\n"
+                            + "|------------------------------------------------|\n"
+                            + "|      1. Buscar libros                          |\n" 
+                            + "|      2. Mostrar los libros disponibles         |\n"
+                            + "|      3. Realizar prestamo de libro             |\n"
+                            + "|      4. Devolver libro prestado                |\n"
+                            + "|      5. Salir                                  |\n"
+                            + "--------------------------------------------------\n"
+                            + "Elija una opción:\n" + reset;
         return menu;
     }
 
 }
-
