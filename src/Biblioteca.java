5cd66be7941a600602a4625eb496d8ff407970d3
Nuel2004 <manurodaspinilla@gmail.com>
2025-01-29 23:18:43 +0100

    este esta buien

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -2,32 +2,6 @@ import java.lang.reflect.GenericSignatureFormatError;
 import java.util.Scanner;
 public class Biblioteca {
     public static final Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
-    public static void main(String[] args) throws Exception {
-        GestorUsuarios gestorU = new GestorUsuarios();
-        gestorU.usuarioNuevo(new Usuarios("Angel", "1234", TipoUsuario.ADMIN));
-        gestorU.usuarioNuevo(new Usuarios("Pablo", "1234", TipoUsuario.ADMIN));
-        gestorU.usuarioNuevo(new Usuarios("Jose", "1234", TipoUsuario.USER));
-        gestorU.usuarioNuevo(new Usuarios("Marta", "rodasbichogrande", TipoUsuario.USER));
-
-        //
-        GestorLibros gestorL = new GestorLibros();
-        gestorL.nuevoLibro(new Libro("El señor de los anillos", "J.R.R. Tolkien", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("El hobbit", "J.R.R. Tolkien", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("Harry Potter y la piedra filosofal", "J.K. Rowling", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("Harry Potter y la camara secreta", "J.K. Rowling", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("Harry Potter y el prisionero de Azkaban", "J.K. Rowling", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("Harry Potter y el caliz de fuego", "J.K. Rowling", Categoria.FANTASIA));
-        gestorL.nuevoLibro(new Libro("Meditaciones", "Marco Aurelio", Categoria.FILOSOFIA));
-        gestorL.nuevoLibro(new Libro("La republica", "Platon", Categoria.FILOSOFIA));
-        gestorL.nuevoLibro(new Libro("Critica de la razon pura", "Immanuel Kant", Categoria.FILOSOFIA));
-        gestorL.nuevoLibro(new Libro("El arte de la guerra", "Sun Tzu", Categoria.FILOSOFIA));
-        gestorL.nuevoLibro(new Libro("La divina comedia", "Dante Alighieri", Categoria.POESIA));
-        gestorL.nuevoLibro(new Libro("Rimas y leyendas", "Gustavo Adolfo Becquer", Categoria.POESIA));
-        gestorL.nuevoLibro(new Libro("Cien años de soledad", "Gabriel Garcia Marquez", Categoria.TERROR));
-        
-        menuPrincipal(gestorU);
-=======
     GestorUsuarios gestor = new GestorUsuarios();
     GestorLibros gestorLibros = new GestorLibros();
     public Biblioteca(GestorUsuarios gestor, GestorLibros gestorLibros) {
@@ -45,7 +19,6 @@ public class Biblioteca {
     }
     public void setGestorLibros(GestorLibros gestorLibros) {
         this.gestorLibros = gestorLibros;
->>>>>>> RamaIntermedia-Rodas
     }
 
     public static void main(String[] args) throws Exception {
@@ -70,18 +43,6 @@ public class Biblioteca {
 
     public void menuPrincipal(GestorUsuarios gestor){
         int opcion;
-<<<<<<< HEAD
-            do{
-                System.out.println(menu());
-                opcion = sc.nextInt();
-                switch(opcion){
-                    case 1 -> menuInicioSesion(gestor);
-                    case 2 -> System.out.println("Saliendo del programa..."+
-                    "\nGracias por confiar en Byte & Books");
-                    default -> System.out.println("Opcion no valida");
-                }
-            }while(opcion !=2);     
-=======
         do{
             System.out.println(menu());
             opcion = Integer.parseInt(sc.nextLine());
@@ -92,7 +53,6 @@ public class Biblioteca {
                 default -> System.out.println("Opcion no valida");
             }
         }while(opcion !=2);     
->>>>>>> RamaIntermedia-Rodas
     }
 
     public static String menu(){
@@ -110,30 +70,7 @@ public class Biblioteca {
                             + "Elija una opción:\n" + reset;
         return menu;
     }
-<<<<<<< HEAD
-    
-    public static void menuInicioSesion(GestorUsuarios gestor){
-        sc.nextLine();
-        System.out.println("Dame tu nombre de usuario");
-        String nombreini = sc.nextLine();
-        System.out.println("Dime tu contraseña");
-        String contrasenaini = sc.nextLine();
-        Usuarios usuinisesion = gestor.inicioSesionUsuarios(nombreini, contrasenaini);
-        if (usuinisesion!=null) {
-            if (usuinisesion.getTipoUsuario()==TipoUsuario.ADMIN) {
-                System.out.println(menuAdmin());
-            }else{
-                System.out.println(menuUser());
-            }
-        }else{
-            System.out.println("ta mal");
-        }
-    }
-
-    public static String menuAdmin(){
-=======
     public static String ImprimirMenuAdmin(){
->>>>>>> RamaIntermedia-Rodas
         String tVerde = "\u001B[32m";
         String fBlanco = "\u001B[32;40m";
         String reset = "\u001B[0m";
@@ -157,10 +94,7 @@ public class Biblioteca {
                             + "Elija una opción:\n" + reset;
         return menu;
     }
-    public static String menuUser(){
-=======
     public static String ImprimirMenuUser(){
-
         String tVerde = "\u001B[32m";
         String fBlanco = "\u001B[32;40m";
         String reset = "\u001B[0m";
